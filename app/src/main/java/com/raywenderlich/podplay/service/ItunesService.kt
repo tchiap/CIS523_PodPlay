9041a549b8f0bdc55403a4d99a2e1fcd87c3cef3
tjc8402 <57610534+tjc8402@users.noreply.github.com>
2020-12-02 11:23:18 -0600

    appears to be working, checking in project

--- app/src/main/java/com/raywenderlich/podplay/service/ItunesService.kt
+++ app/src/main/java/com/raywenderlich/podplay/service/ItunesService.kt
@@ -1,32 +1,34 @@
-
-
 package com.raywenderlich.podplay.service
 
 import retrofit2.Call
 import retrofit2.Retrofit
+
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
+// page 455
+// Chapter 20 networking
+
+
 interface ItunesService {
 
-  // 1
-  @GET("/search?media=podcast")
-  fun searchPodcastByTerm(@Query("term") term: String): Call<PodcastResponse>
+    @GET("/search?media=podcast")
+
+    // 2
+    fun searchPodcastByTerm(@Query("term") term: String): Call<PodcastResponse>
 
 
-  // 3
-  companion object {
+    // 3
+    companion object {
 
-    // 4
-    val instance: ItunesService by lazy {
-      // 5
-      val retrofit = Retrofit.Builder()
-          .baseUrl("https://itunes.apple.com")
-          .addConverterFactory(GsonConverterFactory.create())
-          .build()
+        val instance: ItunesService by lazy {
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://itunes.apple.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
 
-      retrofit.create<ItunesService>(ItunesService::class.java)
+            retrofit.create<ItunesService>(ItunesService::class.java)  // 6
+        }
     }
-  }
 }
--- app/src/main/java/com/raywenderlich/podplay/service/PodcastResponse.kt
+++ app/src/main/java/com/raywenderlich/podplay/service/PodcastResponse.kt
@@ -1,14 +1,16 @@
-
 package com.raywenderlich.podplay.service
+
+// Chapter 20: Networking
+// page 424, 425
 
 data class PodcastResponse(
     val resultCount: Int,
     val results: List<ItunesPodcast>) {
 
-  data class ItunesPodcast(
-      val collectionCensoredName: String,
-      val feedUrl: String,
-      val artworkUrl30: String,
-      val releaseDate: String
-  )
+    data class ItunesPodcast(
+        val collectionCensoredName: String,
+        val feedUrl: String,
+        val artworkUrl100: String,
+        val releaseDate: String
+    )
 }
